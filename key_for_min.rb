861d021b4ca274ef3dd31970c88b8dfa1e81d3f4
Devone Austin <devone.austin@gmail.com>
2020-04-19 04:27:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(hash)
   new_new = []
-  name_hash.collect do |a,b|
+  name_hash.select.collect do |a,b|
   end
 
 end
