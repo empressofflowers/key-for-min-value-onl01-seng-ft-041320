aff8873881c87b834cbb5b4769be53b9c1e045ff
Devone Austin <devone.austin@gmail.com>
2020-04-19 06:22:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-
-  smallest_value_array = []
-  smallest_value_hash = []
-  
-  hash.each.collect { |key, value| smallest_value_array << value.length puts "#{key}, #{value}" }
-  Hash[smallest_value_array]
-
+  new_hash = []
+  *hash[hash.length-1]
 end
