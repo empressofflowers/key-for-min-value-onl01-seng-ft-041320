2468415300d0efac97b1ee51dfcd98edc991827e
Devone Austin <devone.austin@gmail.com>
2020-04-19 05:18:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  new_new = []
-  hash.select.collect do |a,b|
-  end
+  hash.collect { |key, value| if nil return nil}
+  hash
 end
